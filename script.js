5e8efd4f1992af38d3792cd32bbef95582f9c7b3
Zgherea Dumitru <ru.dumit@gmail.com>
2021-03-18 12:18:16 +0100

    game card

---
+++ script.js
@@ -0,0 +1,16 @@
+const favorButton1 = document.getElementById("favoriteButtonId");
+let chechFavorButton = 0;
+
+favorButton1.addEventListener('click', function () {
+    if (chechFavorButton === 0) {
+        favorButton1.classList.remove("favoriteUnchecked");
+        favorButton1.classList.add("favoriteChecked");
+        favorButton1.innerHTML = "✓";
+        return chechFavorButton = 1;
+    } else {
+        favorButton1.classList.remove("favoriteChecked");
+        favorButton1.classList.add("favoriteUnchecked");
+        favorButton1.innerHTML = "+";
+        return chechFavorButton = 0;
+    }
+});
